7eacd22e06b7adbfb1c84390f3ef6cb42069a1b5
GonzaloAxelCode <gonzaloaxelcode@gmail.com>
2023-09-15 00:28:48 -0500

    agregando  modelos de usuario

--- core/urls.py
+++ core/urls.py
@@ -1,7 +1,16 @@
 
+
+from django.conf.urls.static import static
+from django.contrib.sites.models import Site
+
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.conf import settings
+
 
 urlpatterns = [
+    path("auth/", include("djoser.urls")),
+    path("auth/", include("djoser.urls.jwt")),
+    path("auth/", include("apps.user.urls")),
     path('admin/', admin.site.urls),
 ]
